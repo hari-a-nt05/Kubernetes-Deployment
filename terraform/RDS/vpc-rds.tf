8b1793e91d25ff3ed01448627b734dc2a9a33ce0
hari-a-nt05 <hariharan.a@navattech.com>
2026-02-10 15:58:07 +0530

    Updated code

--- terraform/RDS/vpc-rds.tf
+++ terraform/RDS/vpc-rds.tf
@@ -9,3 +9,15 @@ data "aws_vpc" "main" {
     values = ["10.0.0.0/16"]
   }
 }
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.main.id]
+  }
+
+  filter {
+    name   = "cidr-block"
+    values = ["10.0.101.0/24", "10.0.102.0/24"]
+  }
+}
