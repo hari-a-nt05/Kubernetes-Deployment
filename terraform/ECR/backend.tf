45db1b6124470c281947fb07467f752a69592b09
hari-a-nt05 <hariharan.a@navattech.com>
2026-02-10 12:23:30 +0530

    Updated code

--- terraform/ECR/backend.tf
+++ terraform/ECR/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "flask-notes-terraform-state"
+    bucket         = "hari-backend-terraform-state"
     key            = "ecr/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "flask-notes-terraform-locks"
+    dynamodb_table = "hari-backend-terraform-locks"
     encrypt        = true
   }
 }
