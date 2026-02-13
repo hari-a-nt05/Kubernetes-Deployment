0acecaeaec7cc7ac73a6f8813b01be6d8ea75c08
hari-a-nt05 <hariharan.a@navattech.com>
2026-02-13 10:48:10 +0530

    Updated Code

--- terraform/RDS/variables.tf
+++ terraform/RDS/variables.tf
@@ -1,14 +1,17 @@
 variable "db_name" {
+  default = "haridb"
   description = "Database name"
   type        = string
 }
 
 variable "db_username" {
+  default = "hariadmin"
   description = "Master DB username"
   type        = string
 }
 
 variable "db_password" {
+  default = "hari2415"
   description = "Master DB password"
   type        = string
   sensitive   = true
